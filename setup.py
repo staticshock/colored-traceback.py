0773d31d009c91a29532e8abfb10f9b6a45b39ed
Laurie O <laurie_opperman@hotmail.com>
2024-07-11 15:51:47 +1000

    Move project metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
-try:
-    from setuptools import setup
-    has_setuptools = True
-except:
-    from distutils.core import setup
-    has_setuptools = False
+from setuptools import setup
 
-extra_args = {}
-if has_setuptools:
-    extra_args['install_requires'] = ['pygments']
-
-setup(
-    name='colored-traceback',
-    version='0.4.0',
-    description='Automatically color uncaught exception tracebacks',
-    long_description=open("README.rst").read(),
-    author='Anton Backer',
-    author_email='olegov@gmail.com',
-    url='http://www.github.com/staticshock/colored-traceback.py',
-    packages=['colored_traceback', 'colored_traceback.auto', 'colored_traceback.always'],
-    license='ISC',
-    classifiers=(
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-    ),
-    **extra_args
-)
+setup()
